aff6bd38bb90dbe4bc0113fcb3fce3a8e92886d9
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2022-10-05 11:15:25 -0400

    Gameplay ability system player state and controller
    - Added logic for the player state class and the player controller class for the ability system component

--- Source/BattleNetwork/Private/PlayerStates/BNPlayerState.cpp
+++ Source/BattleNetwork/Private/PlayerStates/BNPlayerState.cpp
@@ -3,6 +3,8 @@
 
 #include "PlayerStates/BNPlayerState.h"
 #include "ActorComponents/BNAbilitySystemComponent.h"
+#include "Attributes/BNBaseAttributeSet.h"
+#include "Controllers/BNPlayerController.h"
 
 ABNPlayerState::ABNPlayerState()
 {
@@ -14,6 +16,11 @@ ABNPlayerState::ABNPlayerState()
 	// we won't be told about it by the Server. Attributes, GameplayTags, and GameplayCues will still replicate to us.
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
+	// Create the attribute set, this replicates by default
+	// Adding it as a subobject of the owning actor of an AbilitySystemComponent
+	// automatically registers the AttributeSet with the AbilitySystemComponent
+	BaseAttributeSet = CreateDefaultSubobject<UBNBaseAttributeSet>(TEXT("AttributeSetBase"));
+	
 	// Set PlayerState's NetUpdateFrequency to the same as the Character.
 	// Default is very low for PlayerStates and introduces perceived lag in the ability system.
 	// 100 is probably way too high for a shipping game, you can adjust to fit your needs.
@@ -23,4 +30,53 @@ ABNPlayerState::ABNPlayerState()
 UAbilitySystemComponent* ABNPlayerState::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
+}
+
+UBNBaseAttributeSet* ABNPlayerState::GetAttributeSetBase() const
+{
+	return BaseAttributeSet;
+}
+
+float ABNPlayerState::GetHealth() const
+{
+	return BaseAttributeSet->GetHealth();
+}
+
+float ABNPlayerState::GetMaxHealth() const
+{
+	return BaseAttributeSet->GetMaxHealth();
+}
+
+void ABNPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		// Attribute change callbacks
+		HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(BaseAttributeSet->GetHealthAttribute()).AddUObject(this, &ABNPlayerState::HealthChanged);
+
+	}
+}
+
+void ABNPlayerState::HealthChanged(const FOnAttributeChangeData& Data)
+{
+	/*
+	// Check for and handle knockdown and death
+	AGSHeroCharacter* Hero = Cast<AGSHeroCharacter>(GetPawn());
+	if (IsValid(Hero) && !IsAlive() && !AbilitySystemComponent->HasMatchingGameplayTag(DeadTag))
+	{
+		if (Hero)
+		{
+			if (!AbilitySystemComponent->HasMatchingGameplayTag(KnockedDownTag))
+			{
+				Hero->KnockDown();
+			}
+			else
+			{
+				Hero->FinishDying();
+			}
+		}
+	}
+	*/
 }
--- Source/BattleNetwork/Public/PlayerStates/BNPlayerState.h
+++ Source/BattleNetwork/Public/PlayerStates/BNPlayerState.h
@@ -5,10 +5,14 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
 #include "BNPlayerState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FGSOnGameplayAttributeValueChangedDelegate, FGameplayAttribute, Attribute, float, NewValue, float, OldValue);
 
 class UAbilitySystemComponent;
 class UBNAbilitySystemComponent;
+class UBNBaseAttributeSet;
 
 /**
  * 
@@ -18,14 +22,43 @@ class BATTLENETWORK_API ABNPlayerState : public APlayerState, public IAbilitySys
 {
 	GENERATED_BODY()
 
+public:
+	
 protected:
 
 	UPROPERTY()
 	UBNAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY()
+	UBNBaseAttributeSet* BaseAttributeSet;
+
+	// Attribute changed delegate handles
+	FDelegateHandle HealthChangedDelegateHandle;
+
 
 public:
 
 	ABNPlayerState();
 	
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	UBNBaseAttributeSet* GetAttributeSetBase() const;
+
+	/**
+* Getters for attributes from GDAttributeSetBase. Returns Current Value unless otherwise specified.
+*/
+
+	UFUNCTION(BlueprintCallable, Category = "GASShooter|GSPlayerState|Attributes")
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "GASShooter|GSPlayerState|Attributes")
+	float GetMaxHealth() const;
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Attribute changed callbacks
+	virtual void HealthChanged(const FOnAttributeChangeData& Data);
 };
