f0d2182ead1fc0871d3303f34180dc4559623c6c
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2022-11-10 19:14:26 -0500

    First Pass shoot gameplay ability

---
+++ Source/BattleNetwork/Public/GameplayAbilities/BNGA_FireBullet.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilities/BNBaseGameplayAbility.h"
+#include "BNGA_FireBullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLENETWORK_API UBNGA_FireBullet : public UBNBaseGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<UGameplayEffect> DamageGameplayEffect;
+
+protected:
+
+	// TODO BN: Finish this up
+	// Flip book animation for firing?
+	// We need to be able to grab a projectile and hold onto that as reference?
+	
+public:
+
+	UBNGA_FireBullet();
+
+	/** Actually activate ability, do not call this directly. We'll call it from APAHeroCharacter::ActivateAbilitiesWithTags(). */
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+
+	UFUNCTION()
+	void OnCancelled(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void OnCompleted(FGameplayTag EventTag, FGameplayEventData EventData);
+
+	UFUNCTION()
+	void EventReceived(FGameplayTag EventTag, FGameplayEventData EventData);
+};
