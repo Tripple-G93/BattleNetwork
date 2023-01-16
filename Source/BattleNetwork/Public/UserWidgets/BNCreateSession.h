69b02c72ab242e6201f109b0c48ab496b14a1eaa
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2023-01-16 11:04:51 -0500

    Create Session Widget
    - Created a session widget so that we can use it to hook up the UI logic for the player.
    - Created the base class so that we could use it to bind the widget to C++ code

---
+++ Source/BattleNetwork/Public/UserWidgets/BNCreateSession.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BNCreateSession.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLENETWORK_API UBNCreateSession : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
