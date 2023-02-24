91386cd708f6f6dc65b69f3f4df1a67e552637a7
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2023-02-23 19:56:16 -0500

    Default classes and audio files

---
+++ Source/BattleNetwork/Public/ActorComponents/BNGameMusicAudioComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Components/AudioComponent.h"
+
+#include "Tables/BNGameMusicTable.h"
+
+#include "BNGameMusicAudioComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLENETWORK_API UBNGameMusicAudioComponent : public UAudioComponent
+{
+	GENERATED_BODY()
+
+protected:
+
+    UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UDataTable> GameMusicDataTable;
+
+    TObjectPtr<FBNGameMusicTableInfoRow> CurrentFlipbookAnimationTableInfoRow;
+
+public:
+
+
+	
+};
---
+++ Source/BattleNetwork/Public/Tables/BNGameMusicTable.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "BNGameMusicTable.generated.h"
+
+class USoundCue;
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FBNGameMusicTableInfoRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag GameMusicGameplayTag;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USoundCue> GameMusicSoundBase;
+};
