208d63f264138c3e9ca3656eba0ef609a214a305
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2023-03-30 19:46:40 -0400

    Access single player mode!
    - You can now access single player mode though the camera is wonky and not working correctly

---
+++ Source/BattleNetwork/Public/GameModes/BNGameModeSinglePlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameModes/BNGameModeInitial.h"
+#include "BNGameModeSinglePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLENETWORK_API ABNGameModeSinglePlayer : public ABNGameModeInitial
+{
+	GENERATED_BODY()
+	
+};
