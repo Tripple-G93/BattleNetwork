39f4ba5f1af3459c96505669c71a04b6f7bcddf6
Tripple-G93 <91931425+Tripple-G93@users.noreply.github.com>
2023-04-30 13:18:55 -0400

    Time Remaining To Attack
    - Service created to update this variable
    - Lives in the behavior tree
    - Initialized in the controller

---
+++ Source/BattleNetwork/Public/BTServices/BNBTServiceTimeRemainingToAttack.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BNBTServiceTimeRemainingToAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLENETWORK_API UBNBTServiceTimeRemainingToAttack : public UBTService
+{
+	GENERATED_BODY()
+	
+protected:
+
+    UPROPERTY(EditAnywhere, Category="BN|BlackBoardVariables")
+    FName TimeRemainingToAttackName;
+
+public:
+
+    UBNBTServiceTimeRemainingToAttack(const FObjectInitializer& ObjectInitializer);
+
+protected:
+
+    void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+};
